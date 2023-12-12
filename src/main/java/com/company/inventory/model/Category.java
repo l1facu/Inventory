064e1762557190ae2a8465ef5bf55f81e5227740
facundo <facundoguerra12@gmail.com>
2023-12-12 15:06:41 -0300

    entidad, respuesta

---
+++ src/main/java/com/company/inventory/model/Category.java
@@ -0,0 +1,29 @@
+package com.company.inventory.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name= "category")
+public class Category implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+}
+
+
